33c59ce4a9fe21b1d37af9e5aae852d36bd27448
ses10 <sesma@live.com>
2016-12-26 11:49:12 -0500

    build dictionary from sentiment file

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,15 +1,27 @@
 import sys
 
+def buildDict(sent_file):
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    sent_file.seek(0)
+    return scores
+
 def hw():
     print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
+    fp.seek(0)
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
+
+    sentiments = buildDict(sent_file)
+
     lines(sent_file)
     lines(tweet_file)
 
